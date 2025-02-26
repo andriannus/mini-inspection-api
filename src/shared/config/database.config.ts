84a03d7a769fdabade39e3b6441ef32ac3220047
Andriannus Parasian <andriannus.parasian@efishery.com>
2025-02-26 18:05:23 +0700

    ci: add Profile to deploy to Heroku

--- src/shared/config/database.config.ts
+++ src/shared/config/database.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  dbName: process.env.DATABASE_NAME,
-  uri: `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URI}`,
+  dbName: process.env.MONGO_DATABASE,
+  uri: process.env.MONGO_URI,
 }));
